68146258eae1e2bd34845996bccbb78a22746f53
faineance <faineance@users.noreply.github.com>
2016-02-15 08:40:04 +0000

    ugly

--- src/Main.hs
+++ src/Main.hs
@@ -4,9 +4,6 @@ import qualified Data.Map.Strict as Map
 import           Data.Maybe
 
 
-
-
-
 newtype Context = Context {unctx :: Map.Map String Expr}
 
 
@@ -15,8 +12,11 @@ data Definition = Definition String Expr
 
 data Expr = Int Integer
         | Symbol String
-        | Fn (Context->[Expr]->(Context, Expr))
         | List [Expr]
+        | Builtin Prim
+        | Fn (Context->[Expr]->(Context, Expr))
+
+data Prim = Add | Sub | Mul
 
 eval :: Form -> Context -> (Context, Maybe Expr)
 eval form ctx = case form of
@@ -29,9 +29,17 @@ evalDef (Definition sym expr) (Context ctx) = Context ( Map.insert sym expr ctx)
 evalExpr :: Context -> Expr -> Expr
 evalExpr ctx (Int v) = Int v
 evalExpr ctx (Symbol s) = evalExpr ctx (fromMaybe nil $ Map.lookup s (unctx ctx))
+evalExpr ctx (List []) = nil
+evalExpr ctx (List (x:xs)) = case x of
+    Builtin Add -> Int (sum (map ((\(Int x) -> x ) . evalExpr ctx) xs))
+    Builtin Sub -> Int (foldl (-)
+                            ( (\(Int x) -> x ) $ evalExpr ctx (head xs) )
+                            (map ( (\(Int x) -> x ) . evalExpr ctx) (tail xs) ))
+    Builtin Mul -> Int (product (map ((\(Int x) -> x ) . evalExpr ctx) xs))
 
 nil :: Expr
 nil = List []
+
 main :: IO ()
 main = do
   putStrLn "hello world"
